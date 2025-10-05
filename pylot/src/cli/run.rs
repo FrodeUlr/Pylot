5901889d81ef6a04e71a79e15eb4922d59f087a7
Frode Ulriksen <frode.ulriksen@outlook.com>
2025-10-05 19:02:15 +0200

    .

--- pylot/src/cli/run.rs
+++ pylot/src/cli/run.rs
@@ -155,19 +155,11 @@ mod tests {
 
     #[tokio::test]
     async fn test_delete() {
-        if std::env::var("GITHUB_ACTIONS").is_err() {
-            println!("Skipping test in non-GitHub Actions environment");
-            return;
-        }
         delete(io::stdin(), Some("test_env".to_string()), None).await;
     }
 
     #[tokio::test]
     async fn test_activate() {
-        if std::env::var("GITHUB_ACTIONS").is_err() {
-            println!("Skipping test in non-GitHub Actions environment");
-            return;
-        }
         activate(Some("test_env_not_here".to_string()), None).await;
     }
 
